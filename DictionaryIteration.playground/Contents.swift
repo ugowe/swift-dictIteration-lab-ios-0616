8ca301b22b27f2158bfaaf7d31c8e9779071abb4
ugowe <joe@spheares.com>
2016-07-12 14:56:11 -0400

    add code to Dictionary Iteration lab

--- DictionaryIteration.playground/Contents.swift
+++ DictionaryIteration.playground/Contents.swift
@@ -19,15 +19,17 @@
 // write your code here
 
 
-
-
+var bandsGreatestHits = ["Nirvana": "Nevermind",
+                         "Blondie": "Parallel Lines",
+                         "The Kinks": "Low Budget",
+                         "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+]
 /*: question2
  ### 2. Oops, we forgot an album! Add the Pixie's _Doolittle_ to the dictionary you created in Question 1.
  */
 // write your code here
 
-
-
+bandsGreatestHits["Pixies"] = "Doolittle"
 
 /*: question3
  ### 3. We don't care that much about The Kinks. Delete them from the dictionary you created in Question 1.
@@ -35,8 +37,7 @@
 // write your code here
 
 
-
-
+bandsGreatestHits["The Kinks"] = nil
 
 /*: question4
  ### 4. A band name has been defined in the constant `bandName` below. Using this constant and the dictionary you created in Question 1, print "<Band>'s top-selling album was <Album>" to the console.
@@ -44,29 +45,33 @@
 let bandName = "Nirvana"
 // write your code here
 
-
-
+print("\(bandName)'s top-selling album was \(bandsGreatestHits[bandName]!)")
 
 /*: question5
  ### 5. Iterate over the dictionary you created in Question 1 and print "<Band>'s top-selling album was <Album>" for each item in the dictionary.
  */
 // write your code here
 
-
+for (band, album) in bandsGreatestHits {
+    print("\(band)'s top-selling album was \(album)")
+}
 
 
 /*: question6
  ### 6. What type of data structure is returned when you iterate over a dictionary?
  */
 
-
-
+// Not sure; probably a string
 
 /*: question7
  ### 7. Imagine every student in every grade in a high school took an exam. Each student got a letter grade based on their test results (A, B, C, D, and F). The results were averaged together per grade. How could you represent this mapping of grades to exam average?
  */
-
-
+let grade = ["A": 90..<100,
+             "B": 80..<89,
+             "C": 70..<79,
+             "D": 65..<69
+    
+]
 
 
 /*: question8
@@ -80,14 +85,20 @@ let bandName = "Nirvana"
 // write your code here
 
 
-
+var gradeAndScore = ["Grade 9": "B",
+                     "Grade 10": "A",
+                     "Grade 11": "C",
+                     "Grade 12": "B"
+]
 
 /*: question9
  ### 9. Print the message "Grade <Grade> got <Score>" for each grade in the dictionary you created in Question 8.
  */
 // write your code here
 
-
+for (grade, score) in gradeAndScore {
+    print("\(grade) got a \(score)")
+}
 
 
 /*: question10
